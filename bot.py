941bea08ac62b6207b87822cc8c81e90db3390c6
mrtxni <akunikshannn@gmail.com>
2024-12-05 14:09:57 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,12 +64,12 @@ class Bot(Client):
 	try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            test = await self.send_message(chat_id=db_channel.id, text="Kontol Meledak")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/kucingemas_v for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
