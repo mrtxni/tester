3084f2ec4ec09ab97c76bffa989613ebd9a8ef51
mrtxni <akunikshannn@gmail.com>
2024-12-05 14:21:55 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -119,7 +119,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("🔒 Close", callback_data = "close"),
                 ],
                 [
                     InlineKeyboardButton("⭐ Join Vip", url =f" https://t.me/noturamel"),
