2918602ac9cc94132532cf5ae2d6c06303d12a2e
mrtxni <akunikshannn@gmail.com>
2024-12-05 13:57:16 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,8 +47,9 @@ class Bot(Client):
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
                 self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
-                sys.exit()
-       if FORCE_SUB_CHANNEL2 :
+                sys.exit(),
+
+       if FORCE_SUB_CHANNEL2:
             try:
                 link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
                 if not link:
