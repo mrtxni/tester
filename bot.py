07de8385904d2512891eafd97f3e6778686dd197
mrtxni <akunikshannn@gmail.com>
2024-12-05 13:57:47 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ class Bot(Client):
                 self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
                 sys.exit(),
 
-       if FORCE_SUB_CHANNEL2:
+       if FORCE_SUB_CHANNEL2
             try:
                 link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
                 if not link:
